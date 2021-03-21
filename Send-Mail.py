51d0e958ce2ffc460435884e64b6a8525cd1ab39
HarunBuyuktepe <harun.buyuktepe@gmail.com>
2021-03-22 00:59:47 +0300

    Initial commit

---
+++ Send-Mail.py
@@ -0,0 +1,31 @@
+import smtplib
+import sys
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+
+if True:
+
+    # humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+    # print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+
+    fromaddr = ""
+    toaddr = ""
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Room temperature"
+
+    body = """
+    Hello,
+    """ + "message here"
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP('mail.infomaniak.com', 25)
+    server.starttls()
+    server.login(fromaddr, "pass")
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+    print("Message sent.")
